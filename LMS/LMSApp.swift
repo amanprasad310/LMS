19a9d9d3d4ec5c1c105f4bec562f1ba7958d2f31
Aman Prasad <310amanprasad@gmail.com>
2025-02-15 12:32:54 +0530

    Initial Commit

---
+++ LMS/LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMSApp.swift
+//  LMS
+//
+//  Created by Rounak Azad on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
